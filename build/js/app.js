4d445b8a66ee9ccbe338076ab84c83ce429e3994
Fabián Huertas <miloh9318@hotmail.com>
2025-07-18 14:26:03 -0500

    finalizando modal

--- build/js/app.js
+++ build/js/app.js
@@ -29,7 +29,15 @@ function mostrarImagen(i) {
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
     modal.onclick = cerrarModal;
+
+    // Boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON');
+    cerrarModalBtn.textContent = 'X';
+    cerrarModalBtn.classList.add('btn-cerrar');
+    cerrarModalBtn.onclick = cerrarModal
+
     modal.appendChild(imagen);
+    modal.appendChild(cerrarModalBtn);
 
     // Agregar al HTML
     const body = document.querySelector('body');
